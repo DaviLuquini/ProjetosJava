6ea7f7a13b64cce3445285fae64f0c5a57703f51
Davi de Sá Luquini <davi262016@gmail.com>
2023-08-14 16:15:40 -0300

    git commit -m "Fiz aprimorações no codigo para ele ficar mais interativo com um novo sistema de dinheiro na conta."

--- MegaSena.java
+++ MegaSena.java
@@ -6,13 +6,28 @@ public class MegaSena {
   public static void main(String[] args) {
     int[] numerosApostados = jogarMegaSena();
     int[] numerosSorteados = rodarRoleta();
+    int dinheiroConta = 0;
     impressao(numerosApostados, numerosSorteados);
-    validarNumeros(numerosApostados, numerosSorteados);
+    validarNumeros(numerosApostados, numerosSorteados, dinheiroConta);
   }
 
   public static int[] jogarMegaSena() {
     int[] numerosApostados = new int[6];
     Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite quanto dinheiro você tem:");
+    int dinheiroConta = sc.nextInt();
+    System.out.println("Você deseja comprar a cartela da Mega Sena? Isso custará: 5 Reais");
+    String resposta = sc.next();
+    if (dinheiroConta < 5) {
+      System.out.println("Você não tem dinheiro suficiente.");
+      System.exit(dinheiroConta);
+    } else if (resposta != null && resposta.equalsIgnoreCase("Sim")) {
+      dinheiroConta = (dinheiroConta - 5);
+    } else {
+      System.out.println("Ok :(");
+      System.exit(dinheiroConta);
+    }
 
     System.out.println("Digite os 6 números de 1 a 60 que você deseja apostar:");
 
@@ -58,7 +73,7 @@ public class MegaSena {
     }
   }
 
-  public static void validarNumeros(int[] numerosApostados, int[] numerosSorteados) {
+  public static void validarNumeros(int[] numerosApostados, int[] numerosSorteados, int dinheiroConta) {
     int acerto = 0;
     for (int i = 0; i < 6; i++) {
       if (numerosApostados[i] == numerosSorteados[i]) {
@@ -68,12 +83,19 @@ public class MegaSena {
     System.out.println("\n");
     if (acerto == 4) {
       System.out.println("Parabéns, você acertou 4 números e ganhou 500 mil reais");
+      dinheiroConta = (dinheiroConta + 500000);
+      System.out.println("Agora seu saldo na conta é de: " + dinheiroConta);
     } else if (acerto == 5) {
       System.out.println("Parabéns, você acertou 5 números e ganhou 5 milhões de reais");
+      dinheiroConta = (dinheiroConta + 5000000);
+      System.out.println("Agora seu saldo na conta é de: " + dinheiroConta);
     } else if (acerto == 6) {
       System.out.println("Parabéns, você acertou 5 números e ganhou 200 milhões de reais");
+      dinheiroConta = (dinheiroConta + 200000000);
+      System.out.println("Agora seu saldo na conta é de: " + dinheiroConta);
     } else {
-      System.out.println("Você não acertou nenhum número.");
+      System.out.println("Você não acertou o minímo de 4 números.");
+      System.out.println("Seu saldo na conta continua de: " + dinheiroConta);
     }
   }
 
